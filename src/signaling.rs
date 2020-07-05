6f7f98a169c9a52bddea8ff1e078a6fc9a324692
Will Page <compenguy@gmail.com>
2020-07-04 22:48:37 -0700

    Build fixes for rpi feature

--- src/signaling.rs
+++ src/signaling.rs
@@ -1,4 +1,7 @@
 #[cfg(feature = "rpi")]
+use std::io::{Read, Write};
+
+#[cfg(feature = "rpi")]
 const RED_LED: &str = "/sys/class/leds/led0";
 #[cfg(feature = "rpi")]
 const GREEN_LED: &str = "/sys/class/leds/led1";
@@ -6,7 +9,7 @@ const GREEN_LED: &str = "/sys/class/leds/led1";
 #[cfg(feature = "rpi")]
 fn get_led_state(path: &str) -> u8 {
     let mut file =
-        std::fs::File::open(path).expect(format!("Failed to open led device {} for reading", path));
+        std::fs::File::open(path).expect("Failed to open led device for reading");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect(format!(
         "Failed to read current state of led device {}",
@@ -18,10 +21,10 @@ fn get_led_state(path: &str) -> u8 {
 #[cfg(feature = "rpi")]
 fn set_led_state(path: &str, new_state: u8) {
     let mut out_file = std::fs::File::create(path)
-        .expect(format!("Failed to open led device {} for writing", path));
+        .expect("Failed to open led device for writing");
     out_file
         .write(new_state.to_string())
-        .expect(format!("Failed to write new state for led device {}", path));
+        .expect("Failed to write new state for led device");
 }
 
 #[cfg(feature = "rpi")]
